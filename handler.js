d42666a3d7009fdbe4e9f7a514b62124a8d3570d
bageljp <kadoyama.keisuke@gmail.com>
2017-03-06 18:44:02 +0900

    test code

--- handler.js
+++ handler.js
@@ -14,3 +14,16 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.timestamp = (event, context, callback) => {
+  let d = new Date();
+  let t = d.getFullYear() + '/' + d.getMonth() + '/' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '.' + d.getMilliseconds();
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Get time, " + t,
+    }),
+  };
+  //callback(null, response);
+  setTimeout(callback(null, response), 5000);
+};
